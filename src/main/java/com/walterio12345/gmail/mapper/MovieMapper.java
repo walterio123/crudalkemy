ec073f07542cc24d160fd88192242e524d0276ca
Victor Schweizer <walterio12345@gmail.com>
2022-01-06 12:36:39 -0300

    06-01

--- src/main/java/com/walterio12345/gmail/mapper/MovieMapper.java
+++ src/main/java/com/walterio12345/gmail/mapper/MovieMapper.java
@@ -20,19 +20,19 @@ import javassist.NotFoundException;
 import lombok.var;
 
 @Component
-//@RequiredArgsConstructor
-public class MovieMapper  {
-	@Autowired
-	private CharacterRepository characterRepository;
-	@Autowired
-	private  GenderRepository genderRepository;
-	@Autowired
-	private   GenderMapper genderMapper;
-	@Autowired
-	private  CharacterMapper characterMapper;
-	boolean loadCharacter;
-	
-	static List<MovieDTO> toDTOList( List<Movie> movies) {
+
+public class MovieMapper {
+    @Autowired
+    private CharacterRepository characterRepository;
+    @Autowired
+    private GenderRepository genderRepository;
+    @Autowired
+    private GenderMapper genderMapper;
+    @Autowired
+    private CharacterMapper characterMapper;
+    boolean loadCharacter;
+
+    static List<MovieDTO> toDTOList(List<Movie> movies) {
         return movies.stream().map(movie -> {
             MovieDTO dto = new MovieDTO();
             dto.setId(movie.getId());
@@ -41,90 +41,87 @@ public class MovieMapper  {
             dto.setCreationDate(movie.getCreationDate());
             return dto;
         }).collect(Collectors.toList());
-        
+
     }
-	
-
-	public MovieDTO toDto(Movie movie,boolean loadCharacter) {
-		var dto=new MovieDTO();
-		dto.setId(movie.getId());
-		dto.setCreationDate(movie.getCreationDate());
-		dto.setImage(movie.getImage());
-		dto.setQualification(movie.getQualification());
-		dto.setTitle(movie.getTitle());
-		dto.setGender(genderMapper.toDto(movie.getGender()));
-		//
-		if(loadCharacter){
-			List<CharacterDTO>characterDTOS= (List<CharacterDTO>) characterMapper.toDto((Character) movie.getCharacters(),false);
-			dto.setCharacters(characterDTOS);
-		}
-		//
-		//dto.setCharacters((List<CharacterDTO>) characterMapper.toDto((Character) movie.getCharacters()));
-		return dto;
-	}
 
 
-	public Movie toEntity(MovieDTO dto) throws Exception {
-		Movie movie=new Movie();
-		if (dto.getId() != null) movie.setId((dto.getId()));
-		movie.setId(dto.getId());
-		movie.setCreationDate(dto.getCreationDate());
-		movie.setImage(dto.getImage());
-		movie.setQualification(dto.getQualification());
-		movie.setTitle(dto.getTitle());
-		/////TODO arreglar llegada de datos 
-		System.out.println("ACA VA EL GENERO"+dto.getGenderId());
-		//Gender gender=genderRepository.findById(dto.getId()).orElseThrow(()->new NotFoundException("no Gender id"));
-		Gender gender=genderMapper.toEntity(dto.getGender());
-		//var gender=genderRepository.findById("e161426d-6128-497c-bfe0-c1f549cf2097").orElseThrow(()->new NotFoundException("no Gender id"));
-		movie.setGender(gender);//.orElseThrow(()->new NotFoundException("no Gender id"));
-		////
-		List<Character>charactersList=entityCharacters(dto.getCharactersIds());
+    public MovieDTO toDto(Movie movie, boolean loadCharacter) {
+        var dto = new MovieDTO();
+        dto.setId(movie.getId());
+        dto.setCreationDate(movie.getCreationDate());
+        dto.setImage(movie.getImage());
+        dto.setQualification(movie.getQualification());
+        dto.setTitle(movie.getTitle());
+        dto.setGender(genderMapper.toDto(movie.getGender()));
 
-		movie.setCharacters((Set<Character>) charactersList);//
-		return movie;
-		
-	}
-	public List<Movie>toEntityList(List<MovieDTO>dtos) throws NotFoundException {
-		List<Movie>movies=new ArrayList<>();
-		for (MovieDTO dto:dtos) {
-			Movie movie=new Movie();
-			movie.setId(dto.getId());
-			movie.setTitle(dto.getTitle());
-			movie.setQualification(dto.getQualification());
-			movie.setCreationDate(dto.getCreationDate());
-			Gender gender=genderMapper.toEntity(dto.getGender());
-			movie.setGender(gender);
-			List<Character>characters=entityCharacters(dto.getCharactersIds());
-			movie.setCharacters((Set<Character>) characters);
-			movies.add(movie);
+        if (loadCharacter) {
+            List<CharacterDTO> characterDTOS = (List<CharacterDTO>) characterMapper.toDto((Character) movie.getCharacters(), false);
+            dto.setCharacters(characterDTOS);
+        }
 
-		}
-		return movies;
-	}
-		
+        return dto;
+    }
 
-	    public List<MovieDTO> toDto(List<Movie> movies,Boolean loadCharacter) {
-	        List<MovieDTO>dtos=new ArrayList<>();
-		   for (Movie entity:movies) {
-			   dtos.add(this.toDto(entity,false));
-		   }
 
-		return dtos;
-	    }
+    public Movie toEntity(MovieDTO dto) throws Exception {
+        Movie movie = new Movie();
+        if (dto.getId() != null) movie.setId((dto.getId()));
+        movie.setId(dto.getId());
+        movie.setCreationDate(dto.getCreationDate());
+        movie.setImage(dto.getImage());
+        movie.setQualification(dto.getQualification());
+        movie.setTitle(dto.getTitle());
 
-	  private List<Character> entityCharacters(List<String> charactersIds)  {
-		 List<Character>listaCharacters=new ArrayList<Character>();
-		   for (String characters:charactersIds) {
-		   	Optional<Character>respOptional=characterRepository.findById(characters);
-		   	if(respOptional.isPresent()){
-		   	Character character=respOptional.get();
-		   	listaCharacters.add(character);
-		   }
-		}    
-	        return listaCharacters;
-	    }
 
+        Gender gender = genderMapper.toEntity(dto.getGender());
+        movie.setGender(gender);
+
+        List<Character> charactersList = entityCharacters(dto.getCharactersIds());
+
+        movie.setCharacters((Set<Character>) charactersList);//
+        return movie;
+
+    }
+
+    public List<Movie> toEntityList(List<MovieDTO> dtos) throws NotFoundException {
+        List<Movie> movies = new ArrayList<>();
+        for (MovieDTO dto : dtos) {
+            Movie movie = new Movie();
+            movie.setId(dto.getId());
+            movie.setTitle(dto.getTitle());
+            movie.setQualification(dto.getQualification());
+            movie.setCreationDate(dto.getCreationDate());
+            Gender gender = genderMapper.toEntity(dto.getGender());
+            movie.setGender(gender);
+            List<Character> characters = entityCharacters(dto.getCharactersIds());
+            movie.setCharacters((Set<Character>) characters);
+            movies.add(movie);
+
+        }
+        return movies;
+    }
+
+
+    public List<MovieDTO> toDto(List<Movie> movies, Boolean loadCharacter) {
+        List<MovieDTO> dtos = new ArrayList<>();
+        for (Movie entity : movies) {
+            dtos.add(this.toDto(entity, false));
+        }
+
+        return dtos;
+    }
+
+    private List<Character> entityCharacters(List<String> charactersIds) {
+        List<Character> listaCharacters = new ArrayList<Character>();
+        for (String characters : charactersIds) {
+            Optional<Character> respOptional = characterRepository.findById(characters);
+            if (respOptional.isPresent()) {
+                Character character = respOptional.get();
+                listaCharacters.add(character);
+            }
+        }
+        return listaCharacters;
+    }
 
 
 }
